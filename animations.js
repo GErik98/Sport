bcba603d3e9ffee410390a6714736d2f5f4d13a3
majomavizben <115664122+majomavizben@users.noreply.github.com>
2023-10-16 18:54:39 +0200

    Arculat tervezgetés, egyelőre haszontalan .JS, image folder

---
+++ animations.js
@@ -0,0 +1,32 @@
+// script.js
+const titleSpan = document.getElementById('smp');
+
+// Function to play the animation
+function playAnimation() {
+    anime.timeline({ loop: false })
+        .add({
+            targets: '.title',
+            opacity: [0, 1],
+            translateY: [30, 0],
+            easing: 'easeOutExpo',
+            duration: 1500,
+            delay: anime.stagger(100) // Add a stagger delay for each letter
+        })
+        .add({
+            targets: '.title',
+            opacity: [1, 0],
+            translateY: [0, -30],
+            easing: 'easeInExpo',
+            duration: 1500,
+            delay: anime.stagger(100)
+        });
+}
+
+// Attach the playAnimation function to the hover events
+titleSpan.addEventListener('mouseenter', function() {
+    setTimeout(playAnimation, 300); // Delay the animation by 300 milliseconds
+});
+
+titleSpan.addEventListener('mouseleave', function() {
+    // You can add any additional logic here for when the mouse leaves if needed
+});
--- index.php
+++ index.php
@@ -1,14 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="styles.css" />
   <title>Sport</title>
 </head>
-<body>
-<h1>SPOOOORT</h1>
+<div class="header">
+  <h1 class="nav-title">SportifyMatchPro</h1>
+  <ul class="horizontal-nav">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Services</a></li>
+    <li><a href="#">Contact</a></li>
+  </ul>
+</div>
+<div class="section welcome">
+  <h1>Welcome to <span class="title" id="smp">SMP</span></h1>
+</div>
+
+<div class="section content">
+  <div class="content-wrapper">
+    <h3>Foci</h3>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab cupiditate quidem blanditiis quam! Blanditiis odit,
+      unde aspernatur molestiae rerum corrupti similique modi, impedit corporis iure natus adipisci id quod hic dicta
+      facere illo, maxime placeat suscipit excepturi quas ipsum commodi. Sapiente molestias accusantium asperiores animi
+      et aliquam similique quia suscipit delectus, nostrum in aut sint deleniti magni neque labore ipsum fuga rem autem
+      molestiae consequatur commodi illum veniam. Non assumenda recusandae cumque obcaecati suscipit hic quas quis, ut
+      temporibus provident cupiditate nihil dolorum eligendi molestias minima natus veritatis doloremque odit,
+      praesentium nesciunt, ipsum soluta corporis laborum. Odit provident explicabo iure?
+    </p>
+  </div>
+</div>
+<div class="section content">
+  <div class="content-wrapper">
+    <h3>F1</h3>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut doloremque mollitia ab sit eum dolore. Nesciunt,
+      perferendis? Ullam adipisci distinctio nemo assumenda quisquam, accusantium nam molestias consequatur, facilis hic
+      perferendis dolores quod blanditiis nobis aspernatur doloribus aut in. Ut excepturi architecto, fuga dolore
+      tenetur adipisci odit consectetur temporibus, illo accusamus nemo, blanditiis culpa provident? Officiis cum
+      provident laborum, praesentium nesciunt rem dolorem rerum ullam architecto nihil exercitationem nam! Repudiandae
+      harum molestias quis nisi ducimus aspernatur error tempore et. Nobis explicabo alias enim qui cupiditate deleniti?
+      Cupiditate aliquid eos est soluta? Quos, maxime voluptatum molestiae maiores qui non pariatur suscipit ipsa.
+    </p>
+  </div>
+</div>
+<div class="section content">
+  <div class="content-wrapper">
+    <h3>Tenisz</h3>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quasi ex ipsum explicabo quidem id, soluta,
+      aspernatur temporibus architecto placeat quam ab voluptas cum amet eum, hic optio ducimus! Eveniet, aliquam minus.
+      Dicta ipsa molestias harum enim. Aliquam cumque molestias temporibus saepe animi, illum assumenda quaerat magni
+      reiciendis eveniet alias odio quam expedita, corrupti cum voluptate, porro soluta quasi! Sequi, sed soluta.
+      Aperiam molestias dicta voluptate nisi nesciunt ullam optio officia harum blanditiis adipisci odit voluptates a
+      pariatur reprehenderit fuga cupiditate, minima repellat deserunt nostrum. Odit architecto odio quibusdam totam
+      quas eveniet unde, velit provident, ab dolores quidem expedita cum.
+    </p>
+  </div>
+</div>
 <?php
-echo 'asd';
-?>
+//echo '<br>asd'; ?>
 </body>
+
+<script src="animations.js"></script>
+
 </html>
