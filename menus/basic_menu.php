48eddd50d926e4a80188231a66e744cf951f2f3b
majomavizben <115664122+majomavizben@users.noreply.github.com>
2023-11-21 15:20:48 +0100

    ötletek.txt

--- menus/basic_menu.php
+++ menus/basic_menu.php
@@ -2,9 +2,9 @@
     <a href='#home'><h1 class='nav-title' id='logo'>Sportify</h1></a>
     <ul class='horizontal-nav'>
       <li><a href='#about'>About</a></li>
-      <li><a href='#foci'>Football</a></li>
-      <li><a href='#f1'>Formula 1</a></li>
-      <li><a href='#tenisz'>Tennis</a></li>
+      <li><a href='#foci'>Design</a></li>
+      <li><a href='#f1'>Ease of use</a></li>
+      <li><a href='#tenisz'>Try it</a></li>
       <li><a href='login.php'>Login</a></li>
       <li><a href='register.php'>Register</a></li>
       <li><a href='#contact'>Contact</a></li>
