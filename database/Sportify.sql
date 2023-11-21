0a2189cab1dfe339b7d83d85a27fbd7f72ed8a43
majomavizben <115664122+majomavizben@users.noreply.github.com>
2023-11-21 14:53:16 +0100

    nem tom valamai database hibás cuccli

--- database/Sportify.sql
+++ database/Sportify.sql
@@ -1,27 +1,129 @@
--- --------------------------------------------------------
--- Hoszt:                        127.0.0.1
--- Szerver verzió:               10.4.28-MariaDB - mariadb.org binary distribution
--- Szerver OS:                   Win64
--- HeidiSQL Verzió:              12.4.0.6659
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 21, 2023 at 01:34 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Tábla adatainak mentése sportify.felhasznalo: ~1 rows (hozzávetőleg)
-INSERT INTO `felhasznalo` (`id`, `username`, `password`) VALUES
-	(1, 'admin', 'admin123');
+--
+-- Database: `sportify`
+--
 
--- Tábla adatainak mentése sportify.rendezveny: ~0 rows (hozzávetőleg)
+-- --------------------------------------------------------
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+--
+-- Table structure for table `contacts`
+--
+
+CREATE TABLE `contacts` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `message` longtext NOT NULL,
+  `submission_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `felhasznalo`
+--
+
+CREATE TABLE `felhasznalo` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `felhasznalo`
+--
+
+INSERT INTO `felhasznalo` (`id`, `username`, `password`, `role`) VALUES
+(1, 'admin', '$2y$10$JtyEHoW2GtbWa.VrEoHJ4uIvq21VOY/k2vbYtvXQgEpX/7hxQk1Im', 'admin'),
+(24, 'szervezo', '$2y$10$N4SwfHT5xYNu8Exqv/Ly6.gQAdL5WV3KcpQMTD3hvGopBsucfjr6u', 'szervezo'),
+(26, 'user', '$2y$10$xULNDshsbC5YQ5Z5S0Y1F.1jMHfFKoLJP3HGJm6sIJhgM.eCVpm.y', 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rendezveny`
+--
+
+CREATE TABLE `rendezveny` (
+  `id` int(11) NOT NULL,
+  `nev` varchar(255) NOT NULL,
+  `sportag` enum('foci','tenisz','f1','') NOT NULL,
+  `datetime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `rendezveny`
+--
+
+INSERT INTO `rendezveny` (`id`, `nev`, `sportag`, `datetime`) VALUES
+(1, 'Football Match 1', 'foci', '2023-11-17 15:00:00'),
+(2, 'Tennis Tournament', 'tenisz', '2023-11-18 10:30:00'),
+(3, 'Formula 1 Race', 'f1', '2023-11-19 14:00:00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contacts`
+--
+ALTER TABLE `contacts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `felhasznalo`
+--
+ALTER TABLE `felhasznalo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rendezveny`
+--
+ALTER TABLE `rendezveny`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contacts`
+--
+ALTER TABLE `contacts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `felhasznalo`
+--
+ALTER TABLE `felhasznalo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `rendezveny`
+--
+ALTER TABLE `rendezveny`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
